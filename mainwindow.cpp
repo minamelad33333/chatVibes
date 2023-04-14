1e6b20c1cd3b5ef610525ca4f87077c0f72adece
Mina Melad <102828808+minamelad33333@users.noreply.github.com>
2023-04-14 04:28:31 +0200

    fesss

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,18 +1,21 @@
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->showMaximized();
+    QPixmap senderPic(":/imgs/senderPic.jpg") ;
+    int w = ui->senderPic->width() ;
+    int h = ui->senderPic->height() ;
+    ui->senderPic->setPixmap(senderPic.scaled(w,h,Qt::KeepAspectRatio)) ;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
-
